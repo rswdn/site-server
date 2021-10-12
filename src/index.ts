fe4ef1ec0eebf075573b2ae4f8bbb965ac057d2d
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2021-10-12 21:45:07 +1000

    Connected to octokit api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
-import express from "express";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import {Octokit} from '@octokit/rest'
+import {createTokenAuth} from '@octokit/auth-token';
 
+
+dotenv.config()
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) =>{
-	res.send("Works");
-});
 
 app.listen(port,() =>{
 	// tslint:disable-next-line:no-console
 	console.log(`server listening on port:${port}`);
 
 });
+
+
+const gh = new Octokit({
+	auth: process.env.SECRET_KEY,
+});
+
+const owner = 'RyanSowden';
+const repo = 'fsk';
+const url =  '/repos/{owner}/{repo}/{path}';
+const ref = 'heads/master';
+
+const getContents = async () => {
+	const { data } = await gh.request({
+		owner,
+		repo,
+		url,
+		method: 'GET',
+		path: 'contents',
+	});
+
+	// tslint:disable-next-line:no-console
+	console.log(data)
+}
+
+getContents();
